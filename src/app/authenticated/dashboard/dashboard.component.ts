cfed24388b98843b6291079dd854c62eca775d04
Fernando OR <fernando.or6@gmail.com>
2024-02-23 14:28:45 -0300

    feat: create component pie chart

--- src/app/authenticated/dashboard/dashboard.component.ts
+++ src/app/authenticated/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DataTableComponent } from './components/data-table/data-table.component';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DataTableComponent],
+  imports: [DataTableComponent, PieChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
