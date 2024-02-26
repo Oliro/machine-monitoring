504764b2f47895ef9078735984e2a12d2eb43892
Fernando OR <fernando.or6@gmail.com>
2024-02-26 11:22:56 -0300

    build: install angular material

---
+++ src/app/authenticated/dashboard/components/filter-data/filter-data.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-data.component.html',
+  styleUrl: './filter-data.component.scss'
+})
+export class FilterDataComponent {
+
+  
+}
--- src/app/authenticated/dashboard/dashboard.component.ts
+++ src/app/authenticated/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DataTableComponent } from './components/data-table/data-table.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { FilterDataComponent } from './components/filter-data/filter-data.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DataTableComponent, PieChartComponent, LineChartComponent],
+  imports: [DataTableComponent, PieChartComponent, LineChartComponent, FilterDataComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
