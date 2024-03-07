f3edd5a1c6805e78a0eb6d967e457c3e25d473d3
Fernando OR <fernando.or6@gmail.com>
2024-03-07 18:36:48 -0300

    build: deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@
 
 export const environment = {
   production: false,
-  apiUrlv1: 'http://localhost:3000/',
+  //apiUrlv1: 'http://localhost:3000/',
+  apiUrlv1: 'https://oliro.github.io/machine-monitoring/data.json',
   MAP_LAYER: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
 };
 
